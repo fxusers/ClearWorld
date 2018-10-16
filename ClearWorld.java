0edcea1761775064118748eeab75f07e84bfd50b
Babar <fxusers@mail.ru>
2018-10-16 13:59:03 +0300

    #1
    Очистка от логирования и регена

--- ClearWorld.java
+++ ClearWorld.java
@@ -9,10 +9,8 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ClearWorld extends JavaPlugin {
-   
-   /** Логгер */
-   private Logger log;
+public class ClearWorld extends JavaPlugin 
+{
    /** Конфиг */
    private FileConfiguration cfg;
    /** Файл конфига */
@@ -20,100 +18,35 @@ public class ClearWorld extends JavaPlugin {
    /** Время в днях */
    private long t;
    
-   /** Путь к файлу игроков, которые игнорируются */
-   public static String ign;
-   /** Включено ли? */
-   public static boolean enabled;
    /** Время в миллисекундах */
    public static long time;
-   /** Регенирировать ли? */
-   public static boolean regen;
    
    /** Вызывается при включении */
-   public void onEnable() {
-      enabled = true;
-      log = Logger.getLogger("Minecraft");
+   public void onEnable() 
+   {
       cfgFile = new File(getDataFolder(), "config.yml");
       cfg = YamlConfiguration.loadConfiguration(cfgFile);
       loadCfg(); // Загружаем настройки
       saveCfg(); // Сохраняем настройки
-      ign = getDataFolder() + "\\ignored-players.yml";
-      File f = new File(ign);
-      if (!f.exists()) 
-      {
-         try 
-         {
-            f.createNewFile();
-         } 
-         catch (IOException e) 
-         {
-            e.printStackTrace();
-         }
-      }
       
       PluginManager pm = getServer().getPluginManager();
-      
-      if (pm.getPlugin("WorldGuard") != null) 
-      {
-         log("Using WorldGuard!");
-      } 
-      else 
-      {
-         log("WorldGuard not founded! Disabling..");
-         this.setEnabled(false);
-      }
-      
-      if (regen) 
-      {
-         if (pm.getPlugin("WorldEdit") != null) 
-         {
-            log("Using WorldEdit!");
-         } 
-         else 
-         {
-            log("WorldEdit not founded! Disabling regeneration..");
-            regen = false;
-            saveCfg();
-         }
-      }
-      
+      if (pm.getPlugin("WorldGuard") == null) 
+         return;
+
       new AutoCleaner().start(); // Запускает в отдельном потоке AutoCleaner
-      log("Enabled!");
    }
-   
-   /** Вызывается при выключении */
-   public void onDisable() 
-   {
-      enabled = false;
-      log("Disabled!");
-   }
-   
-   /** Логирует в консоль */
-   private void log(String msg) 
-   {
-      log.info("[ClearWorld] " + msg);
-   }
-   
+
    /** Загружает конфиг */
    private void loadCfg() 
    {
-      t = cfg.getInt("time", 30);
+      t = cfg.getInt("time", 30); // Время в днях
       time = (t * 24 * 3600 * 1000);
-      regen = cfg.getBoolean("regen", false);
    }
    
    /** Сохраняет конфиг */
    private void saveCfg() 
    {
       cfg.set("time", t);
-      cfg.set("regen", regen);
-
-      try 
-      {
-         cfg.save(cfgFile);
-      } 
-      catch (Exception e) 
-      { 
-      }
+      cfg.save(cfgFile);
    }
 }
